c8d936b6ae687643f8780113c552039037f4750c
Lucie Yarish <lucieyarish@hotmail.com>
2023-11-02 06:28:34 +0100

    Feature: create DB & connect to app; add to DB and update it in realtime

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
+const appSettings = {
+  databaseURL:
+    'https://endorsement-app-a1eba-default-rtdb.europe-west1.firebasedatabase.app/',
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const endorsementListInDB = ref(database, 'endorsements');
+
 const publishBtn = document.getElementById('publish-btn');
 const endorsementMessage = document.getElementById('endorsement-message');
 const endorsementList = document.getElementById('endorsement-list');
@@ -5,8 +21,23 @@ const endorsementList = document.getElementById('endorsement-list');
 publishBtn.addEventListener('click', function () {
   let message = endorsementMessage.value;
   clearInput(endorsementMessage);
-  addMessageToList(message);
+  push(endorsementListInDB, message);
 });
+
+onValue(endorsementListInDB, function (snapshot) {
+  let endorsementArr = Object.values(snapshot.val());
+
+  clearEndorsementListEl();
+
+  for (let i = 0; i < endorsementArr.length; i++) {
+    let currentEndorsement = endorsementArr[i];
+    addMessageToList(currentEndorsement);
+  }
+});
+
+function clearEndorsementListEl() {
+  endorsementList.innerHTML = '';
+}
 
 function clearInput(input) {
   input.value = '';
